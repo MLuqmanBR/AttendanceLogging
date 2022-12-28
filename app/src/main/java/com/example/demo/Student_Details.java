0c78a6d8a5c94c9570c727074135af9eb2c1e36f
Ekshetra <102426009+Ekshetra@users.noreply.github.com>
2022-12-28 20:00:00 +0530

    simple ui updated

--- app/src/main/java/com/example/demo/Student_Details.java
+++ app/src/main/java/com/example/demo/Student_Details.java
@@ -4,14 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-import java.util.Objects;
-
-public class Attendance extends AppCompatActivity {
+public class Student_Details extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_attendance);
-        Objects.requireNonNull(getSupportActionBar()).hide();
+        setContentView(R.layout.activity_student_details);
     }
 }
