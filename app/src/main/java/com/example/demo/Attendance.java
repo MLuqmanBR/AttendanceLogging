f95f259ba3406b168591c7152538746e4c91fb8a
Ekshetra <102426009+Ekshetra@users.noreply.github.com>
2022-12-28 18:35:17 +0530

    Initial commit

---
+++ app/src/main/java/com/example/demo/Attendance.java
@@ -0,0 +1,15 @@
+package com.example.demo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+        nfc_contents = (TextView) findViewById(R.id.nfc_contents);
+public class Attendance extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_attendance);
+    }
+}
