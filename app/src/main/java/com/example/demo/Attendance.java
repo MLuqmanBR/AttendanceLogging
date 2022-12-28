d4ef0a2c46f1c0ffb7d140c2abe72aa6cb689e7a
Ekshetra <102426009+Ekshetra@users.noreply.github.com>
2022-12-28 20:49:34 +0530

    updated links for activities

--- app/src/main/java/com/example/demo/Attendance.java
+++ app/src/main/java/com/example/demo/Attendance.java
@@ -2,16 +2,26 @@ package com.example.demo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 import java.util.Objects;
 
 public class Attendance extends AppCompatActivity {
+
+    Button button1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_attendance);
         Objects.requireNonNull(getSupportActionBar()).hide();
+        button1 = findViewById(R.id.button);
+        button1.setOnClickListener(view -> {
+            Intent StudentDetailsIntent = new Intent(this,Attendance.class);
+            startActivity(StudentDetailsIntent);
+        });
+
     }
 }
